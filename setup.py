fff0d965ebf3f77f90ba6906f8ff61c499ed016b
Artem Chynchenko <47349783+artchyn@users.noreply.github.com>
2025-09-09 14:21:13 +0200

    Revert "build: Adding Python 3.13, drop Python 3.9 (#161)" (#166)
    This reverts commit 6fc16583a31ce7b4c3c2ac082bdc05062094ab08.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(os.path.join(here, 'volue_insight_timeseries/VERSION')) as fv:
 
 setup(
     name='volue_insight_timeseries',
-    python_requires='>=3.10, <=3.13',
+    python_requires='>=3.9, <3.13a0',
     packages=find_packages(),
     install_requires=extract_requirements('requirements.txt'),
     tests_require=[
